11cda3b968e2715066c0b9fc68577132fc6c09b2
Lucas Rooyakkers <lrooyakkers@kepler.space>
2021-12-20 15:44:09 -0500

    Template takes path instead of string.

--- template.py
+++ template.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
+from pathlib import Path
 
 def _parse_args():
     ap = argparse.ArgumentParser()
-    ap.add_argument('input', type=str)
+    ap.add_argument('input', type=Path)
 
     return ap.parse_args()
 
 
-def part_1(args):
+def part_1(input):
     pass
 
 
-def part_2(args):
+def part_2(input):
     pass
 
 
 if __name__ == '__main__':
-    input = _parse_args().input.split()
+    input = _parse_args().input.read_text()
     part_1(input)
     part_2(input)
